e781176e2999af1b557b56f0d9e758f070296ce7
Minos Chatzidakis <minos.chatzidakis@bloomreach.com>
2013-05-29 14:03:55 +0000

    ONS-1074: Using JcrHelper to save the session

--- tools/jcr-runners/src/main/java/org/onehippo/forge/embargo/runners/EmbargoDocumentsRemover.java
+++ tools/jcr-runners/src/main/java/org/onehippo/forge/embargo/runners/EmbargoDocumentsRemover.java
@@ -3,6 +3,7 @@ package org.onehippo.forge.embargo.runners;
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 
+import org.onehippo.forge.jcrrunner.JcrHelper;
 import org.onehippo.forge.jcrrunner.plugins.AbstractRunnerPlugin;
 
 public class EmbargoDocumentsRemover extends AbstractRunnerPlugin {
@@ -14,7 +15,7 @@ public class EmbargoDocumentsRemover extends AbstractRunnerPlugin {
             path = node.getPath();
             getLogger().info("Removing node: {}", path);
             node.remove();
-            node.getSession().save();
+            JcrHelper.save();
         } catch (RepositoryException e) {
             getLogger().error("Could not remove document " + path, e);
         }
