b0e2e96e18f29b3748cb44fa100cffb472a7c5d2
Jeroen Reijn <j.reijn@onehippo.com>
2013-08-14 10:07:41 +0000

    ONS-1154: Fix issue with incorrect validator in the default DateDialog of the CMS. Now custom validator that takes the current time (time of opening the dialog as the minimum). Now you can set the date / time back to an earlier time / date then was previous set.

--- cms-addon/frontend/src/main/java/org/onehippo/forge/embargo/frontend/plugins/ScheduleDialog.java
+++ cms-addon/frontend/src/main/java/org/onehippo/forge/embargo/frontend/plugins/ScheduleDialog.java
@@ -15,16 +15,20 @@
  */
 package org.onehippo.forge.embargo.frontend.plugins;
 
+import java.util.Calendar;
 import java.util.Date;
 
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.util.value.IValueMap;
+import org.apache.wicket.validation.validator.DateValidator;
 import org.hippoecm.addon.workflow.CompatibilityWorkflowPlugin.WorkflowAction;
+import org.hippoecm.frontend.plugins.yui.datetime.YuiDateTimeField;
 
-public class ScheduleDialog extends WorkflowAction.DateDialog {
+public class ScheduleDialog extends WorkflowAction.WorkflowDialog {
     @SuppressWarnings("unused")
     private final static String SVN_ID = "$Id$";
 
@@ -33,9 +37,21 @@ public class ScheduleDialog extends WorkflowAction.DateDialog {
     final String title;
 
     public ScheduleDialog(WorkflowAction action, PropertyModel<Date> dateModel, final String title, final String text) {
-        action.super(new ResourceModel(text), dateModel);
+        action.super();
+        Calendar minimum = Calendar.getInstance();
+        minimum.setTime(dateModel.getObject());
+        minimum.set(Calendar.SECOND, 0);
+        minimum.set(Calendar.MILLISECOND, 0);
+        // if you want to round upwards, the following ought to be executed: minimum.add(Calendar.MINUTE, 1);
+        dateModel.setObject(minimum.getTime());
+        add(new Label("question", new ResourceModel(text)));
+        YuiDateTimeField ydtf = new YuiDateTimeField("value", dateModel);
+        // changed from the default WorkflowAction.DateDialog to use the current date/time as a minimal instead of the
+        // previous date found in the schedule.
+        ydtf.add(DateValidator.minimum(Calendar.getInstance().getTime()));
+        add(ydtf);
+        setFocusOnCancel();
         this.title = title;
-
     }
 
     @Override
